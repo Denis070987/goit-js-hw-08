41e8bdacf828f445256fe4f62cd3457c6743dd85
Denis070987 <deniskoms@gmail.com>
2022-11-03 16:23:14 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const divGallery = document.querySelector('.gallery');
+
+function creatGallery(galleryItems) {
+    return galleryItems
+        .map((item) => 
+           `<div class="gallery__item">
+  <a class="gallery__item" 
+  href="${item.original}">
+  <img class="gallery__image" 
+  src="${item.preview}" 
+  alt="${item.description}" />
+</a>
+</div>`).join('');
+    
+}
+
+const addGallery = creatGallery(galleryItems);
+divGallery.innerHTML = addGallery;
+
+const SimpleLightbox = require('simplelightbox')
+    new SimpleLightbox('.gallery__item a', {captionsData: "alt"});
+
+
+
+// console.log(galleryItems);
