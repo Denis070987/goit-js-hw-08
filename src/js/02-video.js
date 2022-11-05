4aa664934eb4ebcee9aec3c73d21cce74f577820
Denis070987 <deniskoms@gmail.com>
2022-11-05 11:10:50 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1200));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+         
