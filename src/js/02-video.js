d24f817d1107e1c187b404ea614a60cc17985680
Denis070987 <deniskoms@gmail.com>
2022-11-08 11:06:20 +0200

    fix task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,7 +9,6 @@ player.on('timeupdate', throttle(onPlay, 1200));
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
-         
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+}
